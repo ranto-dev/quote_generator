d2585e673c529449723c7dc5a9d461cf8f565f09
ranto-dev <rantoandrianandraina@gmail.com>
2025-05-11 17:12:32 +0200

    search UI

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,26 @@
-const quotes = document.querySelector(".quotes")
+const quotes = document.querySelector(".quotes");
+const search_form = document.querySelector("#form");
+const key_word = document.querySelector("#key_word");
 
-fetch("/api/quotes.json", {
-  method: "GET",
-})
-  .then((res) => res.json())
-  .then((res) => {
-    for(let q of res) {
-        quotes.innerHTML += quote(q.quote, q.author)
-    }
-  });
+search_form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  fetch("http://localhost:8080/api/quote", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({ key_word: key_word.value }),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      for (let q of res.message) {
+        quotes.innerHTML += quote(q.citation, q.author);
+      }
+    })
+    .catch((err) => console.log(err));
+  key_word.value = "";
+});
 
 function quote(quote, author) {
   return `
